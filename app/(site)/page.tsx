a8c3c02798ff1a784cb3ce145d24443549d10ab6
priscilla <priscilla.lee.son@gmail.com>
2023-04-18 12:36:07 -0700

    Update apostrophes with escape

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -7,14 +7,14 @@ export default async function Home() {
   return (
     <div className="max-w-5xl mx-auto py-20">
       <h1 className="text-7xl font-extrabold">
-        Hello I'm{" "}
+        Hello I&apos;m{" "}
         <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent">
           Priscilla!
         </span>
       </h1>
       <p className="mt-3 text-xl text-gray-600">
-        I'm a React developer based in Portland, OR. This website was built with
-        Next.js, Typescript, Tailwind CSS, and Sanity.io.
+        I&apos;m a React developer based in Portland, OR. This website was built
+        with Next.js, Typescript, Tailwind CSS, and Sanity.io.
       </p>
       <h2 className="mt-20 font-bold text-gray-700 text-3xl">My projects</h2>
       <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
